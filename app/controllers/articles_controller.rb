7a7afaae59715e5bc95b6188294cb597eb17b4bd
Tom Hopkins <tmh418@gmail.com>
2021-03-09 17:08:36 +0000

    Add ability to create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,12 @@ class ArticlesController < ActionController::Base
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description));
+    @article.save
+    redirect_to @article
+  end
 end
